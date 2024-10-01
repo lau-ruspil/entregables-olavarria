51af7c6bd20b0c11d4916234926023d02d0cfa2f
Lautaro Ruspil <lauruspil@gmail.com>
2024-10-01 16:35:24 -0300

    Modifico el ejercicio entregable del automotor

---
+++ EjercicioAutomotor/Auto.ts
@@ -0,0 +1,40 @@
+export class Auto {
+	private marca: string;
+	private modelo: string;
+	private tipo: number;
+	private patente: string;
+
+	constructor(marca: string, modelo: string, tipo: number, patente: string) {
+		this.marca = marca;
+		this.modelo = modelo;
+		this.tipo = tipo;
+		this.patente = patente;
+	}
+
+	public getInfo(): string {
+		return `
+			Vehiculo: Auto
+				Marca: ${this.marca}
+				Modelo: ${this.modelo}
+				Tipo: ${this.tipo}
+				Patente: ${this.patente}
+		`;
+	}
+
+	// Metodo para acceder a la patente
+	public getPatente(): string {
+		return this.patente;
+	}
+
+	public setInfo(
+		marca: string,
+		modelo: string,
+		tipo: number,
+		patente: string
+	) {
+		this.marca = marca;
+		this.modelo = modelo;
+		this.tipo = tipo;
+		this.patente = patente;
+	}
+}
---
+++ EjercicioAutomotor/Camion.ts
@@ -0,0 +1,40 @@
+export class Camion {
+	private marca: string;
+	private modelo: string;
+	private tipo: number;
+	private patente: string;
+
+	constructor(marca: string, modelo: string, tipo: number, patente: string) {
+		this.marca = marca;
+		this.modelo = modelo;
+		this.tipo = tipo;
+		this.patente = patente;
+	}
+
+	public getInfo(): string {
+		return `
+			Vehiculo: Camion
+				Marca: ${this.marca}
+				Modelo: ${this.modelo}
+				Tipo: ${this.tipo}
+				Patente: ${this.patente}
+		`;
+	}
+
+	// Metodo para acceder a la patente
+	public getPatente(): string {
+		return this.patente;
+	}
+
+	public setInfo(
+		marca: string,
+		modelo: string,
+		tipo: number,
+		patente: string
+	) {
+		this.marca = marca;
+		this.modelo = modelo;
+		this.tipo = tipo;
+		this.patente = patente;
+	}
+}
